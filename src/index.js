a15cf5f11b97eaf8a00a88c77c2dcdc57ceef803
Sai Terala <sai.webm@gmail.com>
2016-07-19 17:29:07 -0700

    Inital commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBoyD7Ypc7qbpgoMEmaSIQA83ALp9cQ3v4';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {videos:[]};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key:API_KEY, term:'surfboard'},(videos) =>{
+      //console.log(videos);
+      this.setState({videos});
+      //same as this.setState({videos:videos})
+    });
+  }
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  };
+}
+
+ReactDOM.render(<App />,document.querySelector('.container'));
